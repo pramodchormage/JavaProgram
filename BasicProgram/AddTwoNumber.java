6273a170a431bc8da376a5649b24cbc2c717bc35
pramodchormage <101545318+pramodchormage@users.noreply.github.com>
2022-03-14 11:49:17 +0530

    Add files via upload

---
+++ BasicProgram/AddTwoNumber.java
@@ -0,0 +1,18 @@
+//single line command
+/* Multiple Line Command*/
+
+//Program for Interger data type.
+public class AddTwoNumber
+{
+Public static void main(String args[])
+{
+int num1=50;
+int num2=23;
+int sum;
+
+sum=num1+num2;
+
+System.out.println(sum);
+
+}
+}
